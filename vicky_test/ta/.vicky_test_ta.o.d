95ed834915b1fe51f7c8daa7f31a94e814f657d9
HRL008 <HRL708002@163.com>
2020-03-15 05:17:33 -0700

    first commit

---
+++ vicky_test/ta/.vicky_test_ta.o.d
@@ -0,0 +1,17 @@
+vicky_test_ta.o: vicky_test_ta.c \
+ /home/hrl/optee/optee_os/out/arm/export-ta_arm32/include/tee_internal_api.h \
+ /home/hrl/optee/optee_os/out/arm/export-ta_arm32/include/tee_api.h \
+ /home/hrl/optee/toolchains/aarch32/lib/gcc/arm-linux-gnueabihf/6.2.1/include/stddef.h \
+ /home/hrl/optee/optee_os/out/arm/export-ta_arm32/include/compiler.h \
+ /home/hrl/optee/optee_os/out/arm/export-ta_arm32/include/tee_api_defines.h \
+ /home/hrl/optee/optee_os/out/arm/export-ta_arm32/include/tee_api_types.h \
+ /home/hrl/optee/optee_os/out/arm/export-ta_arm32/include/stdint.h \
+ /home/hrl/optee/toolchains/aarch32/lib/gcc/arm-linux-gnueabihf/6.2.1/include/stdbool.h \
+ /home/hrl/optee/optee_os/out/arm/export-ta_arm32/include/trace.h \
+ /home/hrl/optee/optee_os/out/arm/export-ta_arm32/include/trace_levels.h \
+ /home/hrl/optee/optee_os/out/arm/export-ta_arm32/include/tee_ta_api.h \
+ /home/hrl/optee/optee_os/out/arm/export-ta_arm32/include/tee_internal_api_extensions.h \
+ /home/hrl/optee/optee_os/out/arm/export-ta_arm32/include/stdio.h \
+ /home/hrl/optee/toolchains/aarch32/lib/gcc/arm-linux-gnueabihf/6.2.1/include/stdarg.h \
+ /home/hrl/optee/optee_os/out/arm/export-ta_arm32/include/tee_api_defines_extensions.h \
+ include/vicky_test_ta.h
